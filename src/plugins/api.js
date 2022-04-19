a09da3c23c449c462161b50bfd1018fbe8463125
devmerces <79695868+joepbuhre@users.noreply.github.com>
2022-04-19 17:20:08 +0200

    Added logout reason

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import {router} from '../router'
 import { useStore } from '../store/main'
 
 const api = axios.create({
@@ -8,7 +9,7 @@ const api = axios.create({
 api.interceptors.request.use(config => {
     const store = useStore()
     store.setLoading(true)
-    if(store.isAuthenticated) {
+    if (store.isAuthenticated) {
         config.headers['authorization'] = `Bearer ${store.getJwt}`
     }
     return config
@@ -18,6 +19,32 @@ api.interceptors.response.use(response => {
     const store = useStore()
     store.setLoading(false)
     return response
+}, error => {
+    const response = error.response
+
+    let query;
+    if(response.status >= 400 && response.status < 500) {
+        query = 'logout'
+    } else {
+        query = 'reason'
+    }
+
+    if ('errors' in response.data && response.data.errors.length > 0) {
+        let code;
+        try {
+            code = response.data.errors[0].extensions.code
+        } catch (error) {
+            code = 'CREDENTIALS_EXPIRED'
+        }
+        router.push({
+            name: 'Login',
+            query: {
+                [query]: code
+            }
+        })
+    }
+
+    return error
 })
 
 export default api
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,10 +10,15 @@
 
             <div class="mt-10 text-center" v-if="!store.isAuthenticated">
                 <h1 class="text-3xl font-bold text-main">Log In</h1>
+                <p class="text-red-700 text-sm font-semibold h-10 border border-red-400 shadow-md shadow-red-100 mt-3 flex items-center justify-center relative hover:bg-red-400 hover:text-white ease duration-200 rounded-sm group " role="button" v-if="errorMessage" @click="error = {}">{{errorMessage}} <fa-icon class="absolute top-2 right-2" icon="xmark" /></p>
                 <div>
                     <BaseForm @submit.prevent="loginUser($event.target.elements)" class="my-4">
                         <template v-slot:input="input">
-                            <BaseInput placeholder="Your email" :required="true" autocomplete="username" />
+                            <BaseInput
+                                placeholder="Your email"
+                                :required="true"
+                                autocomplete="username"
+                            />
                             <BaseInput
                                 placeholder="Your password"
                                 autocomplete="current-password"
@@ -27,7 +32,10 @@
                                     type="submit"
                                     class="bg-blue text-white font-semibold py-3 px-5 w-2/3 rounded-full mt-5"
                                 >Log in</button>
-                                <a :href="loginLink" class="block text-main opacity-70 hover:opacity-100 text-sm mt-1 w-2/3 m-auto border-b border-white hover:border-blue hover:text-blue duration-300">Login met microsoft</a>
+                                <a
+                                    :href="loginLink"
+                                    class="block text-main opacity-70 hover:opacity-100 text-sm mt-1 w-2/3 m-auto border-b border-white hover:border-blue hover:text-blue duration-300"
+                                >Login met microsoft</a>
                             </div>
                         </template>
                     </BaseForm>
@@ -35,19 +43,14 @@
             </div>
             <div class="mt-20" v-if="store.isAuthenticated">
                 <h1 class="text-3xl font-bold text-blue">Logged In!</h1>
-                <h3 class="text-xl mt-2">
-                    Hi {{ user.first_name }}. You're succesfully logged in
-                </h3>
+                <h3 class="text-xl mt-2">Hi {{ user.first_name }}. You're succesfully logged in</h3>
                 <router-link
                     to="/"
                     class="h-10 inline-flex bg-slate-200 w-30 px-3 py-2 rounded-md mt-3 mr-2"
                 >
                     <p class="text-md font-semibold">Continue to home</p>
                 </router-link>
-                <button
-                    @click="logout"
-                    class="rounded-md mt-3 mr-2 text-sm underline"
-                >Logout</button>
+                <button @click="logout" class="rounded-md mt-3 mr-2 text-sm underline">Logout</button>
             </div>
         </div>
     </div>
@@ -70,6 +73,7 @@ export default {
     data() {
         return {
             showform: false,
+            error: {}
         }
     },
     mounted() {
@@ -85,6 +89,7 @@ export default {
             this.showform = !this.showform
         },
         logout() {
+            this.error = this.$route.query
             this.store.logout();
         },
         login() {
@@ -132,6 +137,22 @@ export default {
         },
         user() {
             return this.store.getUser;
+        },
+        errorMessage() {
+            if (Object.keys(this.error).length > 0) {
+                const responses = {
+                    INVALID_CREDENTIALS: `You're logged out, please log in again.`,
+                    fallback: `You have been logged out. Log in again`
+                }
+                try {
+
+                    return responses[error.logout]
+                } catch (error) {
+                    return responses['fallback']
+                }
+            }
+            return false
+
         }
     },
     components: { LoginViewButton, BaseForm, BaseInput }
